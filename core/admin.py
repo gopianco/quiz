b9e48beaedf4f5755146cab31ef9c83649a5b8ea
Giovane Pianco <drt.gpianco@ifood3rd.com>
2022-08-26 23:18:25 -0300

    feat: create question class

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class AdminQuestion(admin.ModelAdmin):
+    list_display = ['description', 'options']
+
+admin.site.register(Question, AdminQuestion)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import json
 
-# Create your models here.
+class Question(models.Model):
+    description = models.CharField('Descrição', max_length=600)
+    options = models.CharField('Alternativas', max_length=200)
+
+    def set_options(self, values):
+        self.options = json.dumps(values)
+
+    def get_options(self):
+        return json.loads(self.options)
+
+    def __str__(self):
+            return self.description
+
+    class Meta:
+        verbose_name = 'Pergunta'
+        verbose_name_plural = 'Perguntas'
